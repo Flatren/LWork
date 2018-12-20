fa5529b610139254c58aaee980abe42123f2ff7f
Кольчурин <sashakolchurin@yandex.ru>
2018-12-20 21:41:09 +0500

    Create

---
+++ zap.ps1
@@ -0,0 +1,5 @@
+set-location "C:\Users\FLATREN\Documents\Visual Studio 2015\Projects\LWork\Debug"
+start-process LWork.exe "1"
+start-process LWork.exe "2"
+start-process LWork.exe "3"
+start-process LWork.exe
